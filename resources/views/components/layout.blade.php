f40202fd6c3db455395e18da2577287acba16e26
waiyanphoneaant <wypa07@gmail.com>
2024-12-21 10:04:38 +0700

    improve meta

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google-adsense-account" content="ca-pub-1618114950811949">
     <meta name="description" content="{{$description ?? 'Blog By Verse'}}">
-    <title>{{$title ? $title:'Blog By Verse'}}</title>
+    <title>{{$title ??'Blog By Verse'}}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
